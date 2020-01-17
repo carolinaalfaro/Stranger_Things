3a4a7a76349bedcdd746da67eecf4c20abba60b3
carolinaalfaro <37843530+carolinaalfaro@users.noreply.github.com>
2020-01-17 15:28:02 -0800

    Add files via upload

---
+++ stranger_thing.rb
@@ -0,0 +1,22 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 160
+live_loop:strange do
+  play :c4
+  play :c4
+  play :c3
+  sleep 0.5
+  play :e4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :c5
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  play :e4
+  sleep 0.5
+end
